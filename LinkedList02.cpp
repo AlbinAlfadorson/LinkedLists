a72c4080f8d2a28a03843286fceacdae13f36d98
Constantine Of Constantinopol <107809836+AlbinAlfadorson@users.noreply.github.com>
2024-01-20 14:38:08 +0300

    Update LinkedList02.cpp

--- LinkedList02.cpp
+++ LinkedList02.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-//------------------------------------------------------------------------------------------------------------
 
 struct A
 {
@@ -17,78 +16,75 @@ struct List
     List* next;
 };
 
-//------------------------------------------------------------------------------------------------------------
+int RandData_for_List(unsigned long& seed); // Метод для занесения в список случайных значений для последующей сортировки
 
-int RandData_for_List(unsigned long &seed); // Метод для занесения в список случайных значений для последующей сортировки
 
-//------------------------------------------------------------------------------------------------------------
-
-void Init(List **begin, int seed, int length, int(*RandData_for_List)(unsigned long &));
-void Print(List *begin, int length);
+void Init(List** begin, int seed, int length, int(*RandData_for_List)(unsigned long&));
+void Print(List* begin, int length);
 
 int main()
 {
-   srand(time(0));
-   
-   List* begin = NULL; // Начало списка (исток)
-   begin = new List;
-   begin->data.key = 0;
-   begin->next = NULL;
-   
-   unsigned long seed  = 1; // Стартовое значение данных для создания выборки рандомных значений в List
-   int length = 3; // Изначальная длинна списка
-   
-   cout << "Пожалуйста, задайте длинну списка или он останется длинной равный 5, по умолчанию" << endl;
-   cin >> length;
-   cout << endl;
-   
-   
-  Init(&begin, seed, length, &RandData_for_List);
-  Print(begin, length);
-   
-   
-   
-   
 
-    getch();
+    List* begin = NULL; // Начало списка (исток)
+    begin = new List;
+    begin->data.key = 0;
+    begin->next = NULL;
+
+    unsigned long seed = 1; // Стартовое значение данных для создания выборки рандомных значений в List
+    int length = 3; // Изначальная длинна списка
+
+    cout << "Пожалуйста, задайте длинну списка или он останется длинной равный 5, по умолчанию" << endl;
+    cin >> length;
+    cout << endl;
+
+
+    Init(&begin, seed, length, &RandData_for_List);
+    Print(begin, length);
+
+
+
+    _getch();
     return 0;
 }
 
-//------------------------------------------------------------------------------------------------------------
-
-int RandData_for_List(unsigned long &seed)
+int RandData_for_List(unsigned long& seed)
 {
-    seed = seed * 1103515245 + 12345; // Линейный конгруэнтный метод
-    return (seed / 65536) % 32782;    // Возвращаем значение в диапазоне 0 - 32776
+    srand(time(0));
+
+    seed = rand();
+    return seed;
 }
 
 
 
-void Init(List **begin, int seed, int length, int (*RandData_for_List)(unsigned long &))
+void Init(List** begin, int seed, int length, int (*RandData_for_List)(unsigned long&))
 {
-   
-   List *end = (*begin);
-   for(int i(length - (length - 1)); i <= length; i++)
-   {
 
-   int RandData_for_List(unsigned long &seed);
-   end->next = new List;
-   end = end->next;
-   end->data.key = seed;
-   end->next = NULL;
-   
-   };
-   
+    List* end = (*begin);
+    for (int i(length - (length - 1)); i <= length; i++)
+    {
+        
+        //int RandData_for_List(unsigned long& seed);
+
+        end->next = new List;
+        end = end->next;
+        end->data.key = (rand() + 1) * (i * (i - 1)) / i;
+        seed = end->data.key;
+        end->next = NULL;
+
+    };
+
 }
 
-void Print(List *begin, int length)
+void Print(List* begin, int length)
 {
-    List *print = begin;
-    
-    for(int i(length - (length - 1)); i <= length; i++)
+    List* print = begin;
+
+    for (int i(length - (length - 1)); i <= length; i++)
     {
         cout << print->data.key << " -> ";
         print = print->next;
     }
     cout << "NULL";
-    
+
+}
