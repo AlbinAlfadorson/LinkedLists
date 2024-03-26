d122414526f5f532978e628464f8f57869f08b69
Constantine Of Constantinopol <107809836+AlbinAlfadorson@users.noreply.github.com>
2024-03-26 20:14:02 +0300

    Update LinkedList02.cpp

--- LinkedList02.cpp
+++ LinkedList02.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <ctime>
 #include <conio.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -19,28 +20,32 @@ struct List
 
 //-----------------------------------------------------------------------------------------------------------
 
-int RandData_for_List(unsigned long& seed, int &i);
+int RandData_for_List(unsigned long& seed, int& i);
 
 //-----------------------------------------------------------------------------------------------------------
 
-void Init(List** begin, unsigned long &seed, int length, int(*RandData_for_List)(unsigned long&, int &));
+void Init(List** begin, unsigned long& seed, int length, int(*RandData_for_List)(unsigned long&, int&));
 void add_begin(List** begin, unsigned long& seed, int& length, int(*RandData_for_List)(unsigned long&, int&));
 void Insert(List** begin, unsigned long& seed, int& length, int(*RandData_for_List)(unsigned long&, int&));
+void SOrtList(List** begin, int **Array, int length);
 void Print(List* begin, int length);
+void DelateListMemorY(List** begin);
 
 //-----------------------------------------------------------------------------------------------------------
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
+    
 
     List* begin = NULL; // Начало списка (исток)
     begin = new List;
     begin->data.key = 0;
     begin->next = NULL;
+    int* Array = new int;
 
     unsigned long seed = 1; // Стартовое значение данных для создания выборки рандомных значений в List
-    int length = 3; // Изначальная длинна списка
+    int length = 1; // Изначальная длинна списка
 
     cout << "Пожалуйста, задайте длинну списка или он останется длинной равный 5, по умолчанию" << endl;
     cin >> length;
@@ -53,10 +58,15 @@ int main()
     add_begin(&begin, seed, length, &RandData_for_List);
     Print(begin, length);
 
-    List* step = new List;
-
     Insert(&begin, seed, length, &RandData_for_List);
     Print(begin, length);
+    
+    SOrtList(&begin, &Array, length);
+    Print(begin, length);
+
+    DelateListMemorY(&begin);
+
+    delete [] Array;
 
     _getch();
     return 0;
@@ -64,7 +74,7 @@ int main()
 
 
 
-int RandData_for_List(unsigned long& seed, int &i)
+int RandData_for_List(unsigned long& seed, int& i)
 {
     srand(time(NULL));
 
@@ -74,13 +84,13 @@ int RandData_for_List(unsigned long& seed, int &i)
 
 //-----------------------------------------------------------------------------------------------------------
 
-void Init(List** begin, unsigned long &seed, int length, int (*RandData_for_List)(unsigned long & , int & ))
+void Init(List** begin, unsigned long& seed, int length, int (*RandData_for_List)(unsigned long&, int&))
 {
 
     List* end = (*begin);
     for (int i(length - (length - 1)); i <= length; i++)
     {
-        
+
         RandData_for_List(seed, i);
 
         end->next = new List;
@@ -125,19 +135,76 @@ void Insert(List** begin, unsigned long& seed, int& length, int(*RandData_for_Li
     cout << endl;
     cin >> ins;
 
+    if (ins >= length)
+    {
+        return;
+    }
+
+
     List* step = new List;
     step = (*begin);
-    for (int i(0); i < ins; i++)
+    for (int i(0); i < ins - 2; i++)
     {
         step = step->next;
 
     }
 
-    INS->next = step;
-    step = INS;
+    INS->next = step->next;
+    step->next = INS;
 
     length++;
 }
+
+void SOrtList(List** begin, int **Array, int length)
+{
+    List* sortList = (*begin);
+
+    if (sortList == NULL || sortList->next == NULL)
+    {
+        return;
+    }
+
+    const int A = length;
+
+    int *Arraytemp = new int;
+
+    List* SWAPdata = (*begin); 
+    long long temp;
+    SWAPdata = (*begin);
+    for (int i(0); i < length; i++)
+    {
+        if (SWAPdata->next != NULL)
+        {
+           
+            (*Array)[i] = SWAPdata->data.key;
+            SWAPdata = SWAPdata->next;
+
+        }
+        
+    }
+
+
+    for (int i(0); i < length; i++)
+    {
+        for (int j(i + 1); j < length; j++)
+        {
+            if ((*Array)[i] > (*Array)[j])
+            {
+                swap((*Array)[i], (*Array)[j]);
+
+            }
+        }
+    }
+
+
+    for (int i(0); i < length; i++)
+    {
+        cout << endl << (*Array)[i] << endl;
+
+    }
+}
+
+
 
 void Print(List* begin, int length)
 {
@@ -150,4 +217,25 @@ void Print(List* begin, int length)
     }
     cout << "NULL";
 
+}
+
+
+void DelateListMemorY(List** begin)
+{
+    List* t;
+    List* L;
+    if ((*begin) == NULL)
+    {
+        return;
+    }
+
+
+    while (begin)
+    {
+        t = (*begin);
+        (*begin) = (*begin)->next;
+        delete t;
+
+    }
+    begin = NULL;
 }
